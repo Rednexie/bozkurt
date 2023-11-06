86583be6e5522a73895e9128c2efc84e1895560e
Rednexie <126991413+Rednexie@users.noreply.github.com>
2023-11-06 15:02:29 +0300

    Update arg.js

--- arg.js
+++ arg.js
@@ -2,18 +2,23 @@ function parseCommandLineArgs(args) {
     const keyValuePairs = {};
 
     for (let i = 2; i < args.length; i++) {
-        const arg = args[i];
+        const arg = args[i]?.trim()
 
         // Check if the argument starts with "--" and contains "="
         if (arg.startsWith("--") && arg.includes("=")) {
             const [key, value] = arg.slice(2).split('=');
             keyValuePairs[key] = value;
         }
+        else if (arg.startsWith("-") && arg.includes("=")) {
+            const [key, value] = arg.slice(1).split('=');
+            keyValuePairs[key] = value;
+        }
+        else if (!arg.includes("=")){
+            args.slice(2).forEach(arg => keyValuePairs[arg] = null);
+        }
     }
 
     return keyValuePairs;
 }
 
-const commandLineArgs = parseCommandLineArgs(process.argv);
-
-console.log(commandLineArgs);
+module.exports = parseCommandLineArgs
